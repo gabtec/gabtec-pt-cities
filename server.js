fc8d6fa07ae0043aa3cdbd9d854aae5f0d90ba1e
Gabriel Martins <gabtec@sapo.pt>
2021-09-20 13:22:10 +0100

    add cli arg to use as standAlone API or as middleware

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
 const app = express();
 
-const PORT = 4002;
+let PORT;
+const args = process.argv.slice(2);
+
+if (args.length > 0) {
+  console.log("StandAlone API Mode...");
+  PORT = Number(args[0]);
+}
+
+if (isNaN(PORT)) {
+  console.log("Deve indicar um número de PORTO válido!");
+  process.exit(2);
+}
+// const helmet = require("helmet");
+// const cors = require("cors");
+
+// const PORT = 4002;
 
 module.exports = app;
 
-app.use(cors());
-app.use(helmet());
+// app.use(cors());
+// app.use(helmet());
 app.use(express.json());
 
 /*
@@ -31,6 +44,8 @@ app.use("/api/v1/concelhos", ConcelhosEndpoints);
 app.use("/api/v1/localidades", LocalidadesEndpoints);
 app.use("/api/v1/codigos-postais", CodigosPostaisEndpoints);
 
-// app.listen(PORT, function () {
-//   console.log(`Simple Server listening on port ${PORT}.`);
-// });
+if (PORT) {
+  app.listen(PORT, function () {
+    console.log(`Simple Server listening on port ${PORT}.`);
+  });
+}
